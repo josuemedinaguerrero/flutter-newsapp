b91f256f6024052adfcc4a39002c17d212b628dd
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2023-05-27 08:27:35 -0500

    diseño tarjetas

--- lib/src/widgets/lista_notificas.dart
+++ lib/src/widgets/lista_notificas.dart
@@ -11,8 +11,128 @@ class ListaNoticias extends StatelessWidget {
     return ListView.builder(
       itemCount: noticias.length,
       itemBuilder: (context, index) {
-        return Text(noticias[index].title);
+        return _Noticia(noticia: noticias[index], index: index);
       },
+    );
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _Noticia({required this.noticia, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _TarjetaTopBar(noticia: noticia, index: index),
+        _TarjetaTitulo(noticia: noticia),
+        _TarjetaImagen(noticia: noticia),
+        _TarjetaBody(noticia: noticia),
+        _TarjetaBotones(),
+        const SizedBox(),
+        const Divider(),
+      ],
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _TarjetaTopBar({required this.noticia, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    final myTheme = Theme.of(context);
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text('${index + 1} ', style: TextStyle(color: myTheme.colorScheme.secondary)),
+          Text(noticia.source.name),
+        ],
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaTitulo({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Text(noticia.title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaImagen({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(topLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+        child: FadeInImage(
+          placeholder: const AssetImage('assets/img/giphy.gif'),
+          image: NetworkImage(noticia.urlToImage),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _TarjetaBody extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaBody({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(noticia.description),
+    );
+  }
+}
+
+class _TarjetaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final myTheme = Theme.of(context);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RawMaterialButton(
+          fillColor: myTheme.colorScheme.secondary,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.star_border),
+          onPressed: () {},
+        ),
+        const SizedBox(),
+        RawMaterialButton(
+          fillColor: Colors.blue,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.more),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
